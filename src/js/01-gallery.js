70fe40d9656afbecd0727847e76ccb239094aaee
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-07-29 06:51:12 +0300

    refactoring gallery js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+//створення посилання на галерею
+const gallery = document.querySelector('.gallery');
 
-console.log(galleryItems);
+//створення і рендер розмітки елементів галереї
+//створюємо порожній масив, який заповнюємо розміткою для кожного елемента, шляхом перебирання масиву об'єктів з файлу - galleryItems
+const items = [];
+
+galleryItems.forEach(({ original, preview, description }) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__link');
+  galleryLink.href = original;
+
+  const galleryImage = document.createElement('img');
+  galleryImage.classList.add('gallery__image');
+  galleryImage.src = preview;
+  galleryImage.setAttribute('title', description);
+  galleryImage.alt = description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImage);
+  items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+// додавання затримки для опису зображення
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
