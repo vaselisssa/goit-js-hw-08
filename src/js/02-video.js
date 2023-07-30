d59819bfb82b049e7df071936b9e167967feb6cb
Svitlana Vasylkovska <vaselisssa@gmail.com>
2023-07-31 00:57:47 +0300

    task 2, 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// ініціалізація плеєра
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// відстежування події timeupdate - оновлення часу відтворення
+// зберігання часу відтворення у локальне сховище з відновленням відтворення зі збереженої позиції під час перезавантаження сторінки
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+const STORAGE_KEY = 'feedback-form-state';
+
+//Функція для зберігання поточних даних
+function getFormData() {
+  return { email: refs.email.value, message: refs.message.value };
+}
+
+// Додаємо слухачів подій на форму
+refs.form.addEventListener('input', throttle(onFormImput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+//функція для зберігання поточних даних
+function getFormData() {
+  return { email: refs.email.value, message: refs.message.value };
+}
+
+// функція для очищення сховища і полів форми (і виведення їх у консоль) при сабміті
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(getFormData());
+  refs.form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+// функція, що зберігає введені данні в локальне сховище
+function onFormImput() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(getFormData()));
+}
+
+//перевіряємо чи записані дані у сховищі, і якщо так запусуємо їх у відповідні поля форми
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  const { email, message } = JSON.parse(savedData);
+  refs.email.value = email;
+  refs.message.value = message;
+}
